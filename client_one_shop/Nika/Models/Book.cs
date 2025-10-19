f265b989d0043038c730a822ef05115c779116a6
Unafuji <unafuji.swdev@gmail.com>
2025-10-19 17:47:26 +0700

    s

---
+++ client_one_shop/Nika/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client_one_shop.Nika.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Name { get; set; } = default!;
+        public string? Author { get; set; }
+        public decimal Price { get; set; }
+        public string? ISBN { get; set; }
+        public DateTime? PublishedDate { get; set; }
+        public DateTime CreatedAtUtc { get; set; }
+    }
+}
