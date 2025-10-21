358d993535bde0d6793d1284a63e0ea2b7263b62
Unafuji <unafuji.swdev@gmail.com>
2025-10-21 20:30:07 +0700

    Update ConnectionString.cs

--- client_one_shop/Connections/ConnectionString.cs
+++ client_one_shop/Connections/ConnectionString.cs
@@ -4,6 +4,7 @@ namespace client_one_shop.Connections
 {
     public class ConnectionString
     {
-        public static string connectionString = "Server=DESKTOP-J1L2733;Database=BookStoreDB;Trusted_Connection=True;TrustServerCertificate=True;";
+        //public static string connectionString = "Server=DESKTOP-J1L2733;Database=BookStoreDB;Trusted_Connection=True;TrustServerCertificate=True;";
+        public static string connectionString = "Server=DESKTOP-J1L2733;Database=InventoryManagement;Trusted_Connection=True;TrustServerCertificate=True;";
     }
 }
